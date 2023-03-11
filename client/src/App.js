9781281e27596c3cd8fd082e79285e9e3b476c19
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-03-11 08:01:33 +0530

    INIT

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      REkart
+       <h1 className="text-3xl font-bold">
+      Hello world!
+    </h1>
     </div>
   );
 }
