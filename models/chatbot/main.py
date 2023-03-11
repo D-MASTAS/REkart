b98a05c33c0332a586f1ee47241b0dd58055e9fb
Sujal <mishrasujal64@gmail.com>
2023-03-11 21:05:08 +0530

    Pickle file loaded

--- models/chatbot/main.py
+++ models/chatbot/main.py
@@ -1,14 +1,17 @@
 from Chatapp import Chat
+import pickle
 
-def get_response():
-    message=input("Hello Earth Lover !! How can I help you ?\n")
+def get_response(message):
     chat_instance=Chat()
     response= (chat_instance.Chat_process(prompt=message))
     return response
 
 
-if __name__=='__main__':
-    get_response()
+pickle.dump(get_response,open('model.pkl','wb'))
+    
+
+
+
     
 
 
