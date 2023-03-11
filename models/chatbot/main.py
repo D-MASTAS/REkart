18dcc3cd0b6a1bed89cce60f31f4554934a5e90b
Sujal <mishrasujal64@gmail.com>
2023-03-11 20:55:17 +0530

    Modified API

--- models/chatbot/main.py
+++ models/chatbot/main.py
@@ -1,14 +1,14 @@
 from Chatapp import Chat
 
-def get_response(message):
+def get_response():
+    message=input("Hello Earth Lover !! How can I help you ?\n")
     chat_instance=Chat()
-    response= (chat_instance.Chat_process(message))
+    response= (chat_instance.Chat_process(prompt=message))
     return response
 
 
 if __name__=='__main__':
-    prompt=input("Hello Earth Lover !! How can I help you ?\n")
-    print(get_response(prompt))
+    get_response()
     
 
 
