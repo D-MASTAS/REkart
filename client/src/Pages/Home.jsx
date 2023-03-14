fa58aba88a6010cee6e7c890a34960223ebfe38e
unknown <meghanamaheshwari1@gmail.com>
2023-03-14 20:04:18 +0530

    changes frontend

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
+
 import React from "react";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Product from "../components/Product";
 import Slider from "../components/Slider";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -13,33 +14,10 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Product />
+      <Newsletter/>
       <Footer />
     </div>
   );
 };
 
 export default Home
-=======
-import Announcement from "./components/Announcement"
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar"
-import Newsletter from "./components/Newsletter";
-import Product from "./components/Product";
-import Slider from "./components/Slider";
-function App(){
-  return (
-    <div className="App">
-     <Announcement/>
-    <Navbar/>
-<Slider/>
-<Product/>
-<Newsletter/>
-<Footer/>
-
-
-    </div>
-  );
-
-  }
-export default App;
->>>>>>> f478483fefd8ee14316fcc8075896de997c75f4f
