5d1c21b1b51a296f57618180ca7e0dac099bdc6a
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-03-14 09:55:03 +0530

    Model UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
+import Upload from './Pages/Upload'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </Router>
     </div>
