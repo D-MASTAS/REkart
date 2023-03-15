944c7f2c8c4a1991ec213013b87a1faaf8897348
unknown <meghanamaheshwari1@gmail.com>
2023-03-15 17:09:13 +0530

    catalouge page changed

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -2,7 +2,96 @@ import React from 'react'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <>
+<div className="flex justify-center pt-10">
+  <div className="pb-10 xl:w-96">
+    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+      <input
+        type="search"
+        className="relative m-0 -mr-px block w-[1%] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby="button-addon3" />
+      <button
+        className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+        type="button"
+        id="button-addon3"
+        data-te-ripple-init>
+        Search
+      </button>
+    </div>
+  </div>
+</div>
+
+<div className="bg-white">
+  <div className="mx-auto max-w-2xl px-4 pb-20 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 className="sr-only">Products</h2>
+
+    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
+      </a>
+
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
+      </a>
+
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+      <a href="#" className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+      </a>
+
+    </div>
+  </div>
+</div>
+</>
   )
 }
 
