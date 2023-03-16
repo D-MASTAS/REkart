93ab7dad986e15c14354a1841657d8e11e053350
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-03-16 17:25:52 +0530

    Model UI

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 
+
 import Newsletter from "../components/Newsletter";
 import Product from "../components/Product";
 import Slider from "../components/Slider";
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Product />
       <Newsletter/>
+  
   
       {/* <Footer /> */}
     </div>
