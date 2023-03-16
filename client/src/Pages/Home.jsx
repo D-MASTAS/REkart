590a6286058c5e7a4d7261902b9e3bf7a1a493bc
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-03-16 17:30:36 +0530

    INIT

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,7 +5,6 @@ import React from 'react'
 import Newsletter from "../components/Newsletter";
 import Product from "../components/Product";
 import Slider from "../components/Slider";
-import Newsletter from "../components/Newsletter"
 
 
 
